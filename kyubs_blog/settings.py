b99be841395e7e393958ddc432c2d2dd1dfc77ca
Kyubs <blouiecubero@gmail.com>
2018-01-06 17:35:11 +0800

    added staticfiles again

--- kyubs_blog/settings.py
+++ kyubs_blog/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = os.environ['KYUBS_BLOG_SECRET_KEY']
 DEBUG = os.environ["DEBUG"] == "True"
 
 ALLOWED_HOSTS = [
+    'localhost',
     'kyubs-blog.ap-southeast-1.elasticbeanstalk.com',
     'kyubs-django.ap-southeast-1.elasticbeanstalk.com'
 ]
@@ -126,5 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
