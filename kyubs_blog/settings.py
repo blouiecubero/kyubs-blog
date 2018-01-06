ee871611cfb131830b6a608b1f874ac875b15ca0
Kyubs <blouiecubero@gmail.com>
2018-01-06 14:43:56 +0800

    Added elasticbeanstalk to allowed hosts

--- kyubs_blog/settings.py
+++ kyubs_blog/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lmev1zrzlg#v^yu=0lgst@43zd_qn$gbfcwumn6km#5j%jcryl'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*.elasticbeanstalk.com']
 
 
 # Application definition
